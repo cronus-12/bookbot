d79b90ce6107d1365d0a500805f1015a21e34f53
Tristen <tristenjordan22@gmail.com>
2025-12-17 13:41:03 -0500

    Added an exit code if the path to the book is provided incorrectly. Added the ability to pass in any book that is downloaded to the directory.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import sys
 from stats import word_count, character_count, sort_character_count
 
-def get_book_text():
-    with open("books/frankenstein.txt") as f:
+try:
+    print(sys.argv[1])
+except IndexError:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
+
+def get_book_text(book_path):
+    with open(book_path) as f:
         return f.read()
 
 def main():
-    text = get_book_text()
+    text = get_book_text(book_path)
 
     word_result = word_count(text)
     print(word_result)
